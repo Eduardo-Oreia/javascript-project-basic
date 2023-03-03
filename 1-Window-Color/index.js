8442352f22fe545211fd0417347e3a1c9a58399f
Eduardo-Oreia <carloswe3st@gmail.com>
2023-03-03 11:47:20 -0300

    final

--- 1-Window-Color/index.js
+++ 1-Window-Color/index.js
@@ -1,1 +1,13 @@
+const nameColor = document.querySelector(".color-name")
+const btnColor = document.getElementById("button-color")
+const color = ["red", "blue", "yellow", "pink", "green", "orange"]
 
+btnColor.addEventListener("click", () => {
+    const numRandom  =  Math.floor(Math.random() * color.length)
+    
+    document.body.style.background = color[numRandom]
+
+    nameColor.innerHTML = color[numRandom]
+
+    // console.log(numRandom)
+})
