b5340f8576e46b00450d135b5b61655a37aca262
Eduardo-Oreia <carloswe3st@gmail.com>
2023-03-07 17:56:16 -0300

    final

---
+++ 4-SideBar/Final/index.js
@@ -0,0 +1,10 @@
+const btnMenu = document.querySelector(".nav-toggle")
+const navLinks = document.querySelector(".links")
+
+btnMenu.addEventListener("click", () =>{
+    navLinks.classList.toggle("links-responsive")
+})
+
+
+
+
